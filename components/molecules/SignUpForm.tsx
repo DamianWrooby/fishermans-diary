fc26f05596e61db4eb48c3083515f5cafbdef2eb
Damian Wróblewski <dwroblewski89@gmail.com>
2020-12-06 13:58:41 +0100

    Add password confirmation to sign up form

--- components/molecules/SignUpForm.tsx
+++ components/molecules/SignUpForm.tsx
@@ -11,21 +11,50 @@ import {
   FormHelperText,
   Box,
 } from '@chakra-ui/react';
+import * as Yup from 'yup';
 // import { fbAuth, gAuth } from '../services/firebase';
 // import Menu from '../components/molecules/Menu';
 // import { useAuth } from '../contexts/authContext';
 
 const SignUpForm: React.ReactNode = () => {
   const [show, setShow] = useState(false);
-  const showPassword = () => setShow(!show);
-
-  function validateName(value) {
-    let error;
-    if (!value) {
-      error = 'Name is required';
+  const [showConfirm, setShowConfirm] = useState(false);
+  const showHidden = (type: string): void => {
+    switch (type) {
+      case 'password':
+        setShow(!show);
+        break;
+      case 'confirm':
+        setShowConfirm(!showConfirm);
+        break;
+      default:
+        console.log('default switch case');
+        break;
     }
-    return error;
-  }
+  };
+  // function validateEmail(value) {
+  //   let error;
+  //   if (!value) {
+  //     error = 'Name is required';
+  //   }
+  //   return error;
+  // }
+
+  const SignUpSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Email address is invalid!')
+      .required('Email address is required!'),
+    password: Yup.string()
+      .min(8, 'Password is too short!')
+      .required('Password is required!')
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+        'Min 8 characters, one uppercase, one lowercase, one number and one special case character'
+      ),
+    confirmPassword: Yup.string()
+      .required('Must confirm password')
+      .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+  });
 
   return (
     <div className="h-screen flex justify-center items-center">
@@ -38,6 +67,7 @@ const SignUpForm: React.ReactNode = () => {
       >
         <Formik
           initialValues={{ email: '', password: '' }}
+          validationSchema={SignUpSchema}
           onSubmit={(values, actions) => {
             setTimeout(() => {
               alert(JSON.stringify(values, null, 2));
@@ -47,40 +77,89 @@ const SignUpForm: React.ReactNode = () => {
         >
           {(props) => (
             <Form>
-              <Field name="email" validate={validateName}>
+              <Field name="email">
                 {({ field, form }) => (
-                  <FormControl
-                    isInvalid={form.errors.email && form.touched.email}
-                    isRequired
-                  >
-                    <Box mb="5">
+                  <Box mb="5">
+                    <FormControl
+                      isInvalid={form.errors.email && form.touched.email}
+                      isRequired
+                    >
                       <FormLabel htmlFor="email">Email address</FormLabel>
                       <Input {...field} id="email" placeholder="email" />
                       <FormErrorMessage mb="5">
-                        Email is required
+                        {props.errors.email}
                       </FormErrorMessage>
-                    </Box>
-                  </FormControl>
+                    </FormControl>
+                  </Box>
                 )}
               </Field>
               <Field name="password">
                 {({ field, form }) => (
-                  <FormControl isRequired>
-                    <FormLabel htmlFor="password">Password</FormLabel>
-                    <InputGroup size="md">
-                      <Input
-                        {...field}
-                        pr="4.5rem"
-                        type={show ? 'text' : 'password'}
-                        placeholder="Enter password"
-                      />
-                      <InputRightElement width="4.5rem">
-                        <Button h="1.75rem" size="sm" onClick={showPassword}>
-                          {show ? 'Hide' : 'Show'}
-                        </Button>
-                      </InputRightElement>
-                    </InputGroup>
-                  </FormControl>
+                  <Box mb="5">
+                    <FormControl
+                      isInvalid={form.errors.password && form.touched.password}
+                      isRequired
+                    >
+                      <FormLabel htmlFor="password">Password</FormLabel>
+                      <InputGroup size="md">
+                        <Input
+                          {...field}
+                          pr="4.5rem"
+                          type={show ? 'text' : 'password'}
+                          placeholder="Enter password"
+                        />
+                        <InputRightElement width="4.5rem">
+                          <Button
+                            h="1.75rem"
+                            size="sm"
+                            onClick={() => showHidden('password')}
+                          >
+                            {show ? 'Hide' : 'Show'}
+                          </Button>
+                        </InputRightElement>
+                      </InputGroup>
+                      <FormErrorMessage mb="5">
+                        {props.errors.password}
+                      </FormErrorMessage>
+                    </FormControl>
+                  </Box>
+                )}
+              </Field>
+              <Field name="confirmPassword">
+                {({ field, form }) => (
+                  <Box mb="5">
+                    <FormControl
+                      isInvalid={
+                        form.errors.confirmPassword &&
+                        form.touched.confirmPassword
+                      }
+                      isRequired
+                    >
+                      <FormLabel htmlFor="confirmPassword">
+                        Confirm password
+                      </FormLabel>
+                      <InputGroup size="md">
+                        <Input
+                          {...field}
+                          pr="4.5rem"
+                          type={showConfirm ? 'text' : 'password'}
+                          placeholder="Confirm password"
+                        />
+                        <InputRightElement width="4.5rem">
+                          <Button
+                            h="1.75rem"
+                            size="sm"
+                            onClick={() => showHidden('confirm')}
+                          >
+                            {showConfirm ? 'Hide' : 'Show'}
+                          </Button>
+                        </InputRightElement>
+                      </InputGroup>
+                      <FormErrorMessage mb="5">
+                        {props.errors.confirmPassword}
+                      </FormErrorMessage>
+                    </FormControl>
+                  </Box>
                 )}
               </Field>
               <Button
@@ -88,6 +167,7 @@ const SignUpForm: React.ReactNode = () => {
                 colorScheme="teal"
                 isLoading={props.isSubmitting}
                 type="submit"
+                isDisabled={!props.isValid}
               >
                 Create account
               </Button>
