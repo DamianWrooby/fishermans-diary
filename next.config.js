42c1c94b9d3122cc1760234822b661e468c32638
Damian Wróblewski <dwroblewski89@gmail.com>
2021-01-21 23:02:32 +0100

    Fix import module issue

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 // next.config.js
 const withTM = require('next-transpile-modules')(['ol']); // pass the modules you would like to see transpiled
 
-module.exports = withTM();
-
-module.exports = {
+module.exports = withTM({
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
-};
+});
+
+// module.exports = {
+//   images: {
+//     domains: ['firebasestorage.googleapis.com'],
+//   },
+// };
