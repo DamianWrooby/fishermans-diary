3a7ca0a0fe0c328ad0920c4b3c4c28e15da45df7
Damian Wróblewski <dwroblewski89@gmail.com>
2021-04-15 18:31:58 +0200

    Add extended chakra theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-
+import theme from '../styles/theme';
 export default class Document extends NextDocument {
   render() {
     return (
@@ -32,7 +32,7 @@ export default class Document extends NextDocument {
           </noscript>
         </Head>
         <body>
-          <ColorModeScript initialColorMode={'system'} />
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
