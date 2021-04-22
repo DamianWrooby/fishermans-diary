fe0c81a3b8a5440a5a33eeb0412b303928ede981
Damian Wróblewski <dwroblewski89@gmail.com>
2021-04-22 19:05:50 +0200

    Add social sharing meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -91,11 +91,31 @@ const Home = (): React.ReactNode => {
   return (
     <Layout>
       <Head>
-        <title>Fisherman&apos;s Diary </title>
+        <title>Fisherman&apos;s Diary</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           name="description"
-          content="Write down your fish in Fisherman's Diary"
+          content="🎣 Write down your fish in Fisherman's Diary"
+        />
+        <meta
+          property="og:url"
+          content="https://fishermans-diary.vercel.app/pl"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Fisherman's Diary" />
+        <meta
+          name="twitter:card"
+          content="Fisherman's Diary is a web application dedicted to anglers. Users can save information about fish species, method, bait etc. by interactive map. It is possible also to display fish caught by society or display place of catch on map."
+        />
+        <meta
+          property="og:description"
+          content="Fisherman's Diary is a web application dedicted to anglers. Users can save information about fish species, method, bait etc. by interactive map. It is possible also to display fish caught by society or display place of catch on map."
+        />
+        <meta
+          property="og:image"
+          content={
+            'https://firebasestorage.googleapis.com/v0/b/fisherman-s-diary.appspot.com/o/images%2Fapp%2Fog-image.png?alt=media&token=c01e50af-9dd4-4964-a93f-4f4cb996ee29'
+          }
         />
       </Head>
       {content}
